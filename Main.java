93d9eb48fc6ce6c0f7ac130876ea40e65144ebd7
100433273EF <100433273@ea.edin.sch.uk>
2021-01-20 14:29:56 +0000

    lol

--- Main.java
+++ Main.java
@@ -1,21 +1,15 @@
 class Main {
   public static void main(String[] args) {
     
-    //Step 1: initialise names array
   
     String [] names = {"Marisol","Gaynelle","Josue","Cletus","Dong","Lucille","Margot","Mai","Vaughn","Sebastian","Petrina","Maxima","Fletcher","Kenda","Doug","Dot","Jeromy","Jerald","Alla","Georgene"};
    
-    //Step 2: ask user for the number to seperate students by
-  
-    //Step 3: Start fixed loop length of names array times
-  
-    //Step 4: if loop counter modulus the number entered is 0 then
-  
-    //Step 5: display the name from the array
-  
-    //Step 6: end if
-  
-    //Step 7: end loop
+    int separation = Keyboard.getInt("Please enter the number to separate students by.");
+    for (int index = 0; index < 20; index ++) {
+      if (((index + 1)%separation) == 0)  {
+        System.out.println(names[index] + " ");
+      }
+    }
 
   }
 }
